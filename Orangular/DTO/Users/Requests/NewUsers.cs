f401c95b68bf69eca0f92a83d100f15758a2ce8d
Junil8 <just-julian@hotmail.com>
2021-09-15 14:06:45 +0200

    Skoledag færdig

--- Orangular/DTO/Users/Requests/NewUsers.cs
+++ Orangular/DTO/Users/Requests/NewUsers.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Orangular.API.Helpers;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,19 @@ namespace Orangular.DTO.Users.Requests
 {
     public class NewUsers
     {
+        // -----------------------------------------------------------------------------------------------------------------------
+        [Required]
+        [StringLength(255, ErrorMessage = "Max string length is 255")]
+        [MinLength(1, ErrorMessage = "Min string length is 1")]
+        public string email { get; set; }
+        // -----------------------------------------------------------------------------------------------------------------------
+        [Required]
+        [StringLength(255, ErrorMessage = "Max string length is 255")]
+        [MinLength(1, ErrorMessage = "Min string length is 1")]
+        public string password { get; set; }
+        // -----------------------------------------------------------------------------------------------------------------------
+        [Required]
+        public Role role { get; set; }
+        // -----------------------------------------------------------------------------------------------------------------------
     }
 }
