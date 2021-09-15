1798867072103b3d8593312862dd6492ab43c5aa
MuhmenP <muhmenpk@gmail.com>
2021-09-15 12:02:22 +0200

    ALL PAGES WITH ROUTING

--- Orangular.Client/src/app/app-routing.module.ts
+++ Orangular.Client/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BasketPageComponent } from './basket-page/basket-page.component';
+import { CategoryPageComponent } from './category-page/category-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { SearchPageComponent } from './search-page/search-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'categories', component: CategoryPageComponent},
+  {path: 'search', component: SearchPageComponent},
+  {path: 'login', component: LoginPageComponent},
+  {path: 'basket', component: BasketPageComponent}
+  
+
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Orangular.Client/src/app/app.module.ts
+++ Orangular.Client/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { CategoryPageComponent } from './category-page/category-page.component';
+import { SearchPageComponent } from './search-page/search-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { RegistrationPageComponent } from './registration-page/registration-page.component';
+import { BasketPageComponent } from './basket-page/basket-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CategoryPageComponent,
+    SearchPageComponent,
+    LoginPageComponent,
+    RegistrationPageComponent,
+    BasketPageComponent
   ],
   imports: [
     BrowserModule,
