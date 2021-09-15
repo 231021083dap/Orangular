454da38013c708c5e98fa3a69afef7b660548c12
Junil8 <just-julian@hotmail.com>
2021-09-15 11:58:02 +0200

    Working on DTOs, just a checkpoint

--- Orangular/DTO/Categories/Requests/NewCategory.cs
+++ Orangular/DTO/Categories/Requests/NewCategory.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,11 @@ namespace Orangular.DTO.Categories.Requests
 {
     public class NewCategory
     {
+        // -----------------------------------------------------------------------------------------------------------------------
+        [Required]
+        [StringLength(255, ErrorMessage = "Max string length is 255")]
+        [MinLength(1, ErrorMessage = "Min string length is 1")]
+        public string category_name { get; set; }
+        // -----------------------------------------------------------------------------------------------------------------------
     }
 }
---
+++ Orangular/DTO/Order_Items/Requests/UpdateOrder_Item.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Orangular.DTO.Order_Items.Requests
+{
+    public class UpdateOrder_Item
+    {
+    }
+}
