2aae8ae2c51b2d4db039ef9ecb6cd7791d9b8d2b
Muhmen Parvaze <muhmenpk@gmail.com>
2021-09-16 09:52:46 +0200

    repositories

---
+++ Orangular/Repositories/addresses/IAddressesRepository.cs
@@ -0,0 +1,17 @@
+﻿using Orangular.Database.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Orangular.Repositories.addresses
+{
+    interface IAddressesRepository
+    {
+        Task<List<Addresses>> getAll();
+        Task<Addresses> getById(int addressesId);
+        Task<Addresses> create(Addresses addresses);
+        Task<Addresses> update(int addressesId, Addresses addresses);
+        Task<Addresses> delete(int addressesId);
+    }
+}
