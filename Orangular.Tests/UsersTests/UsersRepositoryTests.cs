428383a63ab56dd7961a56438dc3f6cd86eddb62
Julian Genevey Jensen <juli522d@elev.tec.dk>
2021-09-17 10:45:49 +0200

    test

---
+++ Orangular.Tests/UsersTests/UsersRepositoryTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Orangular.Database.Entities;
+using Orangular.Repositories.users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orangular.Tests.UsersTests
+{
+    class UsersRepositoryTests
+    {
+        private readonly UserRepository _sut;
+        private readonly OrangularProjectContext _context;
+        private readonly DbContextOptions<OrangularProjectContext> _options;
+        public UsersRepositoryTests()
+        {
+            _options = new DbContextOptionsBuilder<OrangularProjectContext>()
+                .UseInMemoryDatabase(databaseName "")
+        }
+    }
+}
