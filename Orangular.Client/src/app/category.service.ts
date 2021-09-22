e6bc1e3efb8aedd55cac03b0c46c052864f21a53
Muhmen Parvaze <muhmenpk@gmail.com>
2021-09-22 14:14:12 +0200

    fgrdt

--- Orangular.Client/src/app/category.service.ts
+++ Orangular.Client/src/app/category.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Category } from './models';
 import { Observable } from 'rxjs';
@@ -6,17 +6,26 @@ import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
-export class CGategoryService {
+export class CategoryService {
 
   //Storing the url(C# Swagger)
   private apiUrl = 'https://localhost:5001/api/Categories';
+
+  //adding http headers
+  httpOptions = {
+    headers: new HttpHeaders(
+      { 'Content-Type': 'application/json'}
+    )
+  }
+
   constructor(
     private http:HttpClient
   ) 
   { }
 
-  
+
   getCategories(): Observable<Category[]>{
     return this.http.get<Category[]>(this.apiUrl);
   }
 }
+ 
