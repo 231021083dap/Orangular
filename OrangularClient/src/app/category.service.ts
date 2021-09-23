6a719b917c003afe2a60ab28b57d86314ac253d8
Victor Reipur <victor.reipur@gmail.com>
2021-09-23 10:08:43 +0200

    Muhmets Client implementeret

---
+++ OrangularClient/src/app/category.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Category } from './models';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+
+  //Storing the url(C# Swagger)
+  private apiUrl = 'https://localhost:5001/api/Categories';
+
+  //adding http headers
+  httpOptions = {
+    headers: new HttpHeaders(
+      { 'Content-Type': 'application/json'}
+    )
+  }
+
+  constructor(
+    private http:HttpClient
+  ) 
+  { }
+
+
+  getCategories(): Observable<Category[]>{
+    return this.http.get<Category[]>(this.apiUrl);
+  }
+}
+ 
