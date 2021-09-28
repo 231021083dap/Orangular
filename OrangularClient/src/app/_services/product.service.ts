0475d974563d7b4a4971369424875d8790acecdc
Julian Genevey Jensen <juli522d@elev.tec.dk>
2021-09-28 12:58:33 +0200

    09/28/2021 12:58:33

--- OrangularClient/src/app/_services/product.service.ts
+++ OrangularClient/src/app/_services/product.service.ts
@@ -1,4 +1,4 @@
-import { HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Product } from '../_models/product';
@@ -10,7 +10,7 @@ export class ProductService {
 
   private apiUrl = 'https://localhost:5001/api/Product';
   httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json'})};
-  constructor(private http:HttpHeaders) { }
+  constructor(private http:HttpClient) { }
 
   getAllProduct(): Observable<Product[]>{
     return this.http.get<Product[]>(this.apiUrl);
