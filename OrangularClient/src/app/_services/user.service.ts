cc6e8fde72355b7a30e3454d7b8b57050512c934
Victor Reipur <victor.reipur@gmail.com>
2021-09-28 11:23:43 +0200

    09/28/2021 11:23:43

--- OrangularClient/src/app/_services/user.service.ts
+++ OrangularClient/src/app/_services/user.service.ts
@@ -3,22 +3,23 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { User } from '../_models/user';
 import { Observable } from 'rxjs';
 
+
 @Injectable({
   providedIn: 'root'
 })
+
 export class UserService {
   private endPoint = 'https://localhost:5001/api/User';
 
   private httpOptions = {
-    headers: new HttpHeaders({'Content-Type':'application/json'})
-  }
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
 
-  constructor(private http:HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   // GET all users
-  getAuthors(): Observable<User[]>{
-    return this.http.get<User[]>(this.apiUrl);
+  getAuthors(): Observable<User[]> {
+    return this.http.get<User[]>(this.endPoint);
   }
 
-  // POST
 }
