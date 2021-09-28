6896e0c159254ae9d18b3c3349cc8293aec44966
Julian Genevey Jensen <juli522d@elev.tec.dk>
2021-09-28 12:42:08 +0200

    09/28/2021 12:42:08

--- OrangularClient/src/app/_services/product.service.ts
+++ OrangularClient/src/app/_services/product.service.ts
@@ -1,3 +1,4 @@
+import { HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,7 @@ import { Injectable } from '@angular/core';
 })
 export class ProductService {
 
+  private apiUrl = 'https://localhost:5001/api/Product';
+  httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json'})};
   constructor() { }
 }
