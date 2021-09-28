446aa8e72213daf44f1c2394567a6ae110be548f
Julian Genevey Jensen <juli522d@elev.tec.dk>
2021-09-28 10:51:11 +0200

    09/28/2021 10:51:11

---
+++ OrangularClient/src/app/_models/category.ts
@@ -0,0 +1,11 @@
+import {Product} from './product';
+export interface Category{
+    productId:number,
+     breed:string, 
+     price:number,
+      weight:number, 
+      gender:string, 
+      description:string, 
+      categoryId: number,
+      categoryName: Category
+}
