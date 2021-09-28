e90b110338f77d0ad26ec01f09ff8b837fdc896d
Julian Genevey Jensen <juli522d@elev.tec.dk>
2021-09-28 12:20:17 +0200

    09/28/2021 12:20:17

--- OrangularClient/src/app/admin/product/product.component.ts
+++ OrangularClient/src/app/admin/product/product.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../../_models/product'
 
+
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
