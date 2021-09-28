ce6e53a1aee71d45e131a27496bf67eb77bbbdc2
Julian Genevey Jensen <juli522d@elev.tec.dk>
2021-09-28 12:38:01 +0200

    09/28/2021 12:38:01

--- OrangularClient/src/app/app-routing.module.ts
+++ OrangularClient/src/app/app-routing.module.ts
@@ -5,12 +5,10 @@ import { CategoryPageComponent } from './category-page/category-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { SearchPageComponent } from './search-page/search-page.component';
-<<<<<<< HEAD
 import { ProductComponent } from './admin/product/product.component';
-=======
 import { UserComponent } from './admin/user/user.component';
+import { AdminPanelComponent } from './admin-panel/admin-panel.component';
 
->>>>>>> 2b52267aa313599aac918da69251eb50125e023d
 
 const routes: Routes = [
   {path: 'categories', component: CategoryPageComponent},
@@ -18,12 +16,9 @@ const routes: Routes = [
   {path: 'login', component: LoginPageComponent},
   {path: 'product', component: ProductPageComponent},
   {path: 'basket', component: BasketPageComponent},
-<<<<<<< HEAD
   {path: 'admin', component: AdminPanelComponent},
-  {path: 'admin/product', component: ProductComponent}
-=======
+  {path: 'admin/product', component: ProductComponent},
   {path: 'admin/user', component: UserComponent}
->>>>>>> 2b52267aa313599aac918da69251eb50125e023d
 ];
 
 @NgModule({
