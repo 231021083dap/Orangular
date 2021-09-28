c2ad0b24903fa3e3fdef7758f9599676d8121a64
Julian Genevey Jensen <juli522d@elev.tec.dk>
2021-09-28 12:07:18 +0200

    09/28/2021 12:07:18

--- OrangularClient/src/app/app.module.ts
+++ OrangularClient/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -35,7 +36,8 @@ import { ProductComponent } from './admin/product/product.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
