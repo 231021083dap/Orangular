2c1131a8eea1bcff8900c5db1fabaaa1bcff99c0
Victor Reipur <victor.reipur@gmail.com>
2021-10-01 16:46:47 +0200

    10/01/2021 16:46:47

--- OrangularClient/src/app/home-page/home-page.component.ts
+++ OrangularClient/src/app/home-page/home-page.component.ts
@@ -10,29 +10,16 @@ import { ProductService } from '../_services/product.service'
 })
 export class HomePageComponent implements OnInit {
 
-  // public productPicture : string = 'DefaultImage.jpg'
-
-  // public defaultImage: string = `assets/${this.productPicture}`
-
   constructor(private productPillGeneratorService: ProductPillGeneratorService, private productService: ProductService) { }
   public product: Product[] = [];
 
   ngOnInit(): void {
-    console.log(this.productPillGeneratorService.testHelloWorld());
-    //  this.productPillGeneratorService.testGetProductPillContent();
 
-
-    this.productService.getAllProduct().subscribe(a => {
-      this.product = a
-      console.log(this.product);
-
-      console.log(a);
+    this.productService.getAllProduct().subscribe(a => { this.product = a
 
       const parent = document.getElementById('products')
-      // Loop igemmen array
       this.product.forEach(element => {
         
-        console.log(element.id)
         let thisImage = 'DefaultImage.jpg'
 
         if (element.id == 1) {
@@ -48,52 +35,18 @@ export class HomePageComponent implements OnInit {
         newImg.setAttribute('src', `./assets/${thisImage}`)
         newImg.setAttribute('width', '200')
         newImg.setAttribute('height', '200')
-        // width="500" height="600"
         parent!.appendChild(newImg)
 
         const newChildP1 = document.createElement('p')
         newChildP1.innerHTML = "price : 2000"
 
         const newChildP2 = document.createElement('p')
-        setInterval(() => { newChildP2.innerHTML = `name : ${element.breedName}` }, 1000);
+        newChildP2.innerHTML = `name : ${element.breedName}`
         newChildDiv1.appendChild(newChildP1)
         newChildDiv1.appendChild(newChildP2)
       });
-
-      
-
     });
-
-
-
-
-    // setInterval(()=> { console.log(this.product[0].breedName) }, 1000);
-    // console.log("Hello world from ngOnInit()")
-    // document.getElementById("testID")!.innerHTML = "home-page works! Hello world from ngOnInit()"
-    // document.getElementById("testID");
-    // document.getElementById("testID")!.innerHTML = "hey";
-
-    // Dette eksempel viser at vi kan tage et html element og tilføje et child element
-    
-
-
-
-
-
-    // const elementTR = document.createElement("tr");
-    // const edit = document.createElement("td");
-    // edit.setAttribute('class', '_main-createpc-list-pc-table-edit')
-    // edit.setAttribute('id', `_main-createpc-list-pc-table-edit-${list.list[i].serialnr}`)
-    // edit.innerHTML = 'Rediger'
-    // elementTR.appendChild(edit)
-    // document.getElementById('_main-createpc-list-pc-table').appendChild(elementTR)
   }
-
-
-  myConsoleLog(myString: string): void {
-    console.log(myString)
-  }
-
 }
 
 
